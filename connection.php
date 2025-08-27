6051a7458c0608e05cc1e77bf959eb62f9c16b40
Jodi Jonatan <jdi102350@gmail.com>
2025-08-27 20:38:40 +0700

    learn crade

--- connection.php
+++ connection.php
@@ -11,6 +11,16 @@ $connection = mysqli_connect($host, $user, $password, $database);
 if (!$connection) {
     die("Gagal koneksi ke database");
 }
+
+$nama = $_POST['nama'];
+$kelas = $_POST['kelas'];
+$jurusan = $_POST['jurusan'];
+
+if(isset($_POST['simpan'])) {
+    $sql = "insert into siswa (nama, kelas, jurusan) values ('$nama', '$kelas', '$jurusan')";
+    mysqli_query($connection, $sql);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +31,30 @@ if (!$connection) {
     <title>Data siswa</title>
 </head>
 <body>
-    
+    <!-- masukkan data -->
+     <form action="" method="POST">
+        <div>
+            <label>Nama:
+                <input type="text" name="nama" value="<?= $nama ?>">
+            </label>
+        </div>
+        <div>
+            <label>Kelas:
+                <input type="text" name="kelas" value="<?= $kelas ?>">
+            </label>
+        </div>
+        <div>
+            <select name="" id="">
+                <option value="">- Pilih Jurusan -</option>
+                <option value="Rekayasa Perangkat Lunak" <?php if($jurusan == "Rekayasa Perangkat Lunak") echo "selected" ?>>Rekayasa Perangkat Lunak</option>
+                <option value="Teknik Jaringan Akses" <?php if($jurusan == "Teknik Jaringan Akses") echo "selected" ?>>Teknik Jaringan Akses</option>
+                <option value="Teknik Komputer Jaringan" <?php if($jurusan == "Teknik Komputer Jaringan") echo "selected" ?>>Teknik Komputer Jaringan</option>
+                <option value="Desain Komunikasi Visual" <?php if($jurusan == "Desain Komunikasi Visual") echo "selected" ?>>Desain Komunikasi Visual</option>
+            </select>
+        </div>
+        <div>
+            <input type="submit" name="simpan" value="Simpan Data">
+        </div>
+     </form>
 </body>
 </html>
